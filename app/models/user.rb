c406d0089dba7c11d2dfc13e8396cda59360690c
franclobo <fjbl2788@gmail.com>
2023-12-29 19:21:08 -0500

    Add confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # user doesn't need to confirm email, confirmable should be nil
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable, :confirmable: nil
 
   validates :name, presence: true
 
